0f55c13a41f26ffe692606d368f4d788754b84a4
nickcatalano5 <nickcatalano24@gmail.com>
2024-02-08 16:00:47 -0500

    adjusts server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,6 @@ const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware,
 });
 
 // Create a new instance of an Apollo server with the GraphQL schema
